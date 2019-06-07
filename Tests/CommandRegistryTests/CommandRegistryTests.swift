934be39c2475b4e4f654f3af917e3e4f25e9b6fc
Geoffrey Foster <geoff@g-Off.net>
2019-06-07 00:32:05 -0400

    allow passing of args on run

--- Tests/CommandRegistryTests/CommandRegistryTests.swift
+++ Tests/CommandRegistryTests/CommandRegistryTests.swift
@@ -6,7 +6,6 @@ final class CommandRegistryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CommandRegistry().text, "Hello, World!")
     }
 
     static var allTests = [
